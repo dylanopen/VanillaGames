d5114f941007995bda23fe93ce4ab5fcde7e398a
dylancode <opensource@dylancode.dev>
2026-07-19 10:20:52 +0100

    update to 26.2; add & change dependencies

--- src/main/java/org/vanillacraft/vanillagames/VanillaGames.java
+++ src/main/java/org/vanillacraft/vanillagames/VanillaGames.java
@@ -4,7 +4,7 @@ import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.dylancode.craftgui.CraftGui;
+import com.lapisdev.craftgui.CraftGui;
 import org.vanillacraft.vanillagames.command.game.GameCommandRegistry;
 import org.vanillacraft.vanillagames.listener.VanillaGamesListener;
 import org.vanillacraft.vanillagames.command.party.PartyCommandRegistry;
